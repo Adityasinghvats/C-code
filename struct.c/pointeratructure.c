80ed58b10fbdb22518e91cc01df3b656b7d9d63a
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2024-04-13 23:30:56 +0530

    Better syntax

--- struct.c/pointeratructure.c
+++ struct.c/pointeratructure.c
@@ -19,9 +19,10 @@
  typedef struct student{
         char name[15];
         float attendance;
-        int roll;}student;
+        int roll;
+        }student;
         student Aditya;
-int main (){
+    int main (){
     student* x = &Aditya;
     (*x).roll = 4;/*To change value using pointer*/
     printf("%d",Aditya.roll);
